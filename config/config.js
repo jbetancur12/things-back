332537053f8b52acce64ad3bb996d6af0e45949d
Jorge Betancur <jabetancur12@gmail.com>
2022-07-28 00:27:37 +0000

    Back Ready

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 5000,
+  jwtSecret: process.env.JWT_SECRET || 'YOUR_secret_key',
+  mongoUri:
+    process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' +
+    (process.env.IP || '192.168.0.6') +
+    ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernprojectv1',
+};
+
+export default config;
---
+++ express.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+
+import userRoutes from './server/routes/user.routes.js';
+import authRoutes from './server/routes/auth.routes.js';
+
+
+const app = express();
+/*... configure express ... */
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+app.use('/', userRoutes);
+app.use('/', authRoutes);
+
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: err.name + ': ' + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ': ' + err.message });
+    console.log(err);
+  }
+});
+
+export default app;
