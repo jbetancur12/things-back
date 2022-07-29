580c443ff3dfcf4e55f273a01838b4dabce623b3
Jorge Betancur <jabetancur12@gmail.com>
2022-07-29 18:45:06 +0000

    fix(mqtt): mqtt files

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-import config from './config/config.js';
-import app from './express.js';
-import mongoose from 'mongoose';
+import config from './config/config.js'
+import app from './express.js'
+import mongoose from 'mongoose'
+import MqttHandler from './server/services/mqtt_handler.js'
+
+const mqttClient = new MqttHandler()
+mqttClient.connect()
+
+mongoose.Promise = global.Promise
+mongoose
+  .connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to the database!')
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database!', err)
+    process.exit()
+  })
+
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+})
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.log(err);
+    console.log(err)
   }
-  console.info('Server started on port %s.', config.port);
-});
-
-mongoose.Promise = global.Promise;
-mongoose.connect(config.mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  console.info('Server started on port %s.', config.port)
+  mqttClient.subscribe([
+    'topic_sensor_temperature',
+    'topic_sensor_humidity',
+    'json'
+  ])
 })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
-
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`);
-});
-
