ca556ce11688c6a03a899311a571cba7d8f6e490
Jorge Betancur <jabetancur12@gmail.com>
2022-07-29 18:17:02 +0000

    feat(mqtt): added mqtt feature

--- express.js
+++ express.js
@@ -1,31 +1,33 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-import userRoutes from './server/routes/user.routes.js';
-import authRoutes from './server/routes/auth.routes.js';
+import userRoutes from './server/routes/user.routes.js'
+import authRoutes from './server/routes/auth.routes.js'
+import MqttHandler from './server/services/mqtt_handler.js'
 
+const app = express()
+/* ... configure express ... */
 
-const app = express();
-/*... configure express ... */
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const mqttClient = new MqttHandler()
+mqttClient.connect()
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-app.use('/', userRoutes);
-app.use('/', authRoutes);
-
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to bezkoder application.' })
+})
+app.use('/', userRoutes)
+app.use('/', authRoutes)
 
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
-    res.status(401).json({ error: err.name + ': ' + err.message });
+    res.status(401).json({ error: err.name + ': ' + err.message })
   } else if (err) {
-    res.status(400).json({ error: err.name + ': ' + err.message });
-    console.log(err);
+    res.status(400).json({ error: err.name + ': ' + err.message })
+    console.log(err)
   }
-});
+})
 
-export default app;
+export default app
