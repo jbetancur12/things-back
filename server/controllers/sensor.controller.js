c7785a880849e4006d42cfbd92dbcfebe172b96d
Jorge Betancur <jabetancur12@gmail.com>
2022-07-29 21:01:05 +0000

    feat(sensor.controller): added range dates to query

--- server/controllers/sensor.controller.js
+++ server/controllers/sensor.controller.js
@@ -5,6 +5,14 @@ const getByPeriod = async (req, res) => {
   try {
     const measures = await Measurement.aggregate([
       {
+        $match: {
+          createdAt: {
+            $gte: new Date(req.query.startDate),
+            $lte: new Date(req.query.endDate)
+          }
+        }
+      },
+      {
         $group: {
           _id: {
             $dateTrunc: { date: '$createdAt', unit: 'minute', binSize: 15 }
@@ -12,7 +20,8 @@ const getByPeriod = async (req, res) => {
           averageT: { $avg: '$temperature' },
           averageH: { $avg: '$humidity' }
         }
-      }
+      },
+      { $sort: { _id: 1 } }
     ])
     res.json(measures)
   } catch (err) {
