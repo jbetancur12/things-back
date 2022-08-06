b352aa25b98a640c1aeb0c504e5d34cd620ad982
Jorge Betancur <jabetancur12@gmail.com>
2022-08-06 02:39:04 +0000

    feat(thing): added update operation

--- server/controllers/thing.controller.js
+++ server/controllers/thing.controller.js
@@ -1,5 +1,24 @@
 import Thing from '../models/thing.model.js'
 import errorHandler from '../helpers/dbErrorHandler.js'
+import extend from 'lodash/extend.js'
+
+const thingByID = async (req, res, next, id) => {
+  try {
+    const thing = await Thing.findById(id)
+    console.log(thing)
+    if (!thing) {
+      return res.status('400').json({
+        error: 'Thing not found'
+      })
+    }
+    req.thing = thing
+    next()
+  } catch (err) {
+    return res.status('400').json({
+      error: 'Could not retrieve thing'
+    })
+  }
+}
 
 const create = async (req, res) => {
   const thing = new Thing(req.body)
@@ -7,7 +26,8 @@ const create = async (req, res) => {
   try {
     await thing.save()
     return res.status(200).json({
-      message: 'Successfully signed up!'
+      message: 'Successfully created!',
+      data: thing
     })
   } catch (err) {
     return res.status(400).json({
@@ -18,7 +38,7 @@ const create = async (req, res) => {
 
 const list = async (req, res) => {
   try {
-    const things = await Thing.find().select()
+    const things = await Thing.find()
     res.json(things)
   } catch (err) {
     return res.status(400).json({
@@ -27,4 +47,31 @@ const list = async (req, res) => {
   }
 }
 
-export default { create, list }
+const update = async (req, res) => {
+  try {
+    let thing = req.thing
+    thing = extend(thing, req.body)
+    console.log(thing)
+    thing.updated = Date.now()
+    await thing.save()
+    res.json(thing)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
+
+const remove = async (req, res) => {
+  try {
+    const thing = req.thing
+    const deletedThing = await thing.remove()
+    res.json(deletedThing)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
+
+export default { thingByID, create, list, remove, update }
