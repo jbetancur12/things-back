2b20fcb4f2b4c9162c1b2a990ad897dfbf39fe7d
Jorge Betancur <jabetancur12@gmail.com>
2022-08-27 15:22:52 +0000

    feat(helmet): hetlemt installed

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 
 import userRoutes from './server/routes/user.routes.js'
 import authRoutes from './server/routes/auth.routes.js'
@@ -12,6 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(helmet())
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to bezkoder application.' })
