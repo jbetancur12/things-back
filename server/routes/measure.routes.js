ab25d8610cca9372340b9f58096d8a9c276f8a78
Jorge Betancur <jabetancur12@gmail.com>
2023-04-29 21:11:24 +0000

    feat: create routes to create Varibales, templates etc

---
+++ server/routes/measure.routes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+// import authController from '../controllers/auth.controller.js'
+import measureController from '../controllers/measure.controller.js'
+// import authJwt from '../middlewares/authJwt.js'
+
+const router = express.Router()
+
+router
+  .route('/api/measures')
+  .get(measureController.list)
+  .post(measureController.create)
+
+// router.route('/api/measure/user/:userId').get(measureController.findByUserId)
+
+router
+  .route('/api/measures/:measureId')
+  .get(measureController.find)
+  .put(measureController.update)
+  .delete(measureController.remove)
+
+router.param('measureId', measureController.measureByID)
+export default router
