ed99934c646c24de30bc2b9bdfc3c801929f2027
Jorge Betancur <jabetancur12@gmail.com>
2023-05-17 02:37:45 +0000

    feat(ad): ada

--- server/controllers/measure.controller.js
+++ server/controllers/measure.controller.js
@@ -4,112 +4,114 @@ import errorHandler from '../helpers/dbErrorHandler.js'
 
 const Measure = mongoose.model('Measure')
 
-// const getByPeriod = async (req, res) => {
-//     try {
-//         const startDate = new Date(req.query.startDate)
-//         const endDate = new Date(req.query.endDate)
-//         const timeDiff = endDate - startDate // Diferencia de tiempo en milisegundos
+const getByPeriod = async (req, res) => {
+  try {
+    const startDate = new Date(req.query.startDate)
+    const endDate = new Date(req.query.endDate)
+    const timeDiff = endDate - startDate // Diferencia de tiempo en milisegundos
 
-//         const unitConfigurations = [
-//             { timeLimit: 1 * 60 * 60 * 1000, unit: 'minute', binSize: 10 },
-//             { timeLimit: 6 * 60 * 60 * 1000, unit: 'minute', binSize: 30 },
-//             { timeLimit: 24 * 60 * 60 * 1000, unit: 'minute', binSize: 60 },
-//             { timeLimit: 7 * 24 * 60 * 60 * 1000, unit: 'day', binSize: 1 },
-//         ];
+    const unitConfigurations = [
+      { timeLimit: 1 * 60 * 60 * 1000, unit: 'minute', binSize: 10 },
+      { timeLimit: 6 * 60 * 60 * 1000, unit: 'minute', binSize: 30 },
+      { timeLimit: 24 * 60 * 60 * 1000, unit: 'minute', binSize: 60 },
+      { timeLimit: 7 * 24 * 60 * 60 * 1000, unit: 'day', binSize: 1 }
+    ]
 
-//         const { unit, binSize } = unitConfigurations.find(config => timeDiff <= config.timeLimit);
+    const { unit, binSize } = unitConfigurations.find(
+      (config) => timeDiff <= config.timeLimit
+    )
 
-//         const pipeline = [
-//             {
-//                 $lookup: {
-//                   from: 'variables',
-//                   localField: 'variable',
-//                   foreignField: '_id',
-//                   as: 'variable'
-//                 }
-//               },
-//             {
-//                 $match: {
-//                     $and: [
-//                         { createdAt: { $gte: startDate, $lte: endDate } },
-//                         { template: mongoose.Types.ObjectId(req.query.template) }
-//                     ]
-//                 }
-//             },
-//             {
-//                 $addFields: {
-//                     roundedTimestamp: {
-//                         $dateTrunc: {
-//                             date: '$createdAt',
-//                             unit,
-//                             binSize
-//                         }
-//                     },
-//                     numericValue: {
-//                         $convert: { input: '$value', to: 'double', onError: 0 }
-//                     },
-//                     variableName: { $arrayElemAt: ['$variable.name', 0] },
-//                     variableUnit: { $arrayElemAt: ['$variable.unit', 0] }
-//                 }
-//             },
-//             {
-//                 $group: {
-//                     _id: {
-//                         variableName: '$variableName',
-//                         variableUnit: '$variableUnit',
-//                         roundedTimestamp: '$roundedTimestamp'
-//                     },
-//                     avgValue: {
-//                         $avg: '$numericValue'
-//                     }
-//                 }
-//             },
-//             {
-//                 $group: {
-//                     _id: '$_id.roundedTimestamp',
-//                     units: {
-//                         $push: {
-//                             k: { $arrayElemAt: ['$_id.variableName', 0] },
-//                             v: { $arrayElemAt: ['$_id.variableUnit', 0] }
-//                         }
-//                     },
-//                     measurements: {
-//                         $push: {
-//                             k: { $arrayElemAt: ['$_id.variableName', 0] },
-//                             v: '$avgValue'
-//                         }
-//                     }
-//                 }
-//             },
-//             {
-//                 $project: {
-//                     _id: 0,
-//                     timestamp: '$_id',
-//                     units: {
-//                         $arrayToObject: '$units'
-//                     },
-//                     measurements: {
-//                         $arrayToObject: '$measurements'
-//                     }
-//                 }
-//             },
-//             {
-//                 $sort: {
-//                     timestamp: 1
-//                 }
-//             }
-//         ];
+    const pipeline = [
+      {
+        $lookup: {
+          from: 'variables',
+          localField: 'variable',
+          foreignField: '_id',
+          as: 'variable'
+        }
+      },
+      {
+        $match: {
+          $and: [
+            { createdAt: { $gte: startDate, $lte: endDate } },
+            { template: mongoose.Types.ObjectId(req.query.template) }
+          ]
+        }
+      },
+      {
+        $addFields: {
+          roundedTimestamp: {
+            $dateTrunc: {
+              date: '$createdAt',
+              unit,
+              binSize
+            }
+          },
+          numericValue: {
+            $convert: { input: '$value', to: 'double', onError: 0 }
+          },
+          variableName: '$variable.name',
+          variableUnit: '$variable.unit'
+        }
+      },
+      {
+        $group: {
+          _id: {
+            variableName: '$variableName',
+            variableUnit: '$variableUnit',
+            roundedTimestamp: '$roundedTimestamp'
+          },
+          avgValue: {
+            $avg: '$numericValue'
+          }
+        }
+      },
+      {
+        $group: {
+          _id: '$_id.roundedTimestamp',
+          units: {
+            $push: {
+              k: { $arrayElemAt: ['$_id.variableName', 0] },
+              v: { $arrayElemAt: ['$_id.variableUnit', 0] }
+            }
+          },
+          measurements: {
+            $push: {
+              k: { $arrayElemAt: ['$_id.variableName', 0] },
+              v: '$avgValue'
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          timestamp: '$_id',
+          units: {
+            $arrayToObject: '$units'
+          },
+          measurements: {
+            $arrayToObject: '$measurements'
+          }
+        }
+      },
+      {
+        $sort: {
+          timestamp: 1
+        }
+      }
+    ]
 
-//         const m = await Measure.aggregate(pipeline)
+    const m = await Measure.aggregate(pipeline)
 
-//         res.json(m)
-//     } catch (err) {
-//         console.log(err)
-//         return res.status(400).json({
-//             error: errorHandler.getErrorMessage(err)
-//         })
-//     }
-// }
+    res.json(m)
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
 
 // const getByPeriod = async (req, res) => {
 //     try {
@@ -400,154 +402,154 @@ const Measure = mongoose.model('Measure')
 //     }
 // }
 
-const getByPeriod = async (req, res) => {
-  try {
-    const startTime = performance.now() // Registra el tiempo de finalización
-    const interval = 24 * 60 * 60 * 1000
-    const startDate = new Date(req.query.startDate)
-    const endDate = new Date(req.query.endDate)
-    const timeDiff = endDate - startDate
+// const getByPeriod = async (req, res) => {
+//   try {
+//     const startTime = performance.now() // Registra el tiempo de finalización
+//     const interval = 24 * 60 * 60 * 1000
+//     const startDate = new Date(req.query.startDate)
+//     const endDate = new Date(req.query.endDate)
+//     const timeDiff = endDate - startDate
 
-    let unit, binSize
+//     let unit, binSize
 
-    if (timeDiff <= 1 * 60 * 60 * 1000) {
-      unit = 'minute'
-      binSize = 10
-    } else if (timeDiff <= 6 * 60 * 60 * 1000) {
-      unit = 'minute'
-      binSize = 30
-    } else if (timeDiff <= 24 * 60 * 60 * 1000) {
-      unit = 'minute'
-      binSize = 60
-    } else if (timeDiff <= 7 * 24 * 60 * 60 * 1000) {
-      unit = 'day'
-      binSize = 1
-    } else {
-      unit = 'day'
-      binSize = 7
-    }
+//     if (timeDiff <= 1 * 60 * 60 * 1000) {
+//       unit = 'minute'
+//       binSize = 10
+//     } else if (timeDiff <= 6 * 60 * 60 * 1000) {
+//       unit = 'minute'
+//       binSize = 30
+//     } else if (timeDiff <= 24 * 60 * 60 * 1000) {
+//       unit = 'minute'
+//       binSize = 60
+//     } else if (timeDiff <= 7 * 24 * 60 * 60 * 1000) {
+//       unit = 'day'
+//       binSize = 1
+//     } else {
+//       unit = 'day'
+//       binSize = 7
+//     }
 
-    const pipeline = [
-      {
-        $lookup: {
-          from: 'variables',
-          localField: 'variable',
-          foreignField: '_id',
-          as: 'variable'
-        }
-      },
-      {
-        $match: {
-          $and: [
-            {
-              createdAt: {
-                $gte: startDate,
-                $lte: endDate
-              }
-            },
-            { template: mongoose.Types.ObjectId(req.query.template) }
-          ]
-        }
-      },
-      {
-        $addFields: {
-          roundedTimestamp: {
-            $dateTrunc: {
-              date: '$createdAt',
-              unit,
-              binSize
-            }
-          },
-          numericValue: {
-            $convert: { input: '$value', to: 'double', onError: 0 }
-          },
-          variableName: '$variable.name',
-          variableUnit: '$variable.unit'
-        }
-      },
-      {
-        $group: {
-          _id: {
-            variableName: '$variableName',
-            variableUnit: '$variableUnit',
-            roundedTimestamp: '$roundedTimestamp'
-          },
-          avgValue: {
-            $avg: '$numericValue'
-          }
-        }
-      },
-      {
-        $group: {
-          _id: '$_id.roundedTimestamp',
-          units: {
-            $push: {
-              k: '$_id.variableName',
-              v: '$_id.variableUnit'
-            }
-          },
-          measurements: {
-            $push: {
-              k: '$_id.variableName',
-              v: '$avgValue'
-            }
-          }
-        }
-      },
-      {
-        $project: {
-          _id: 0,
-          timestamp: '$_id',
-          units: {
-            $arrayToObject: '$units'
-          },
-          measurements: {
-            $arrayToObject: '$measurements'
-          }
-        }
-      },
-      {
-        $sort: {
-          timestamp: 1
-        }
-      }
-    ]
+//     const pipeline = [
+//       {
+//         $lookup: {
+//           from: 'variables',
+//           localField: 'variable',
+//           foreignField: '_id',
+//           as: 'variable'
+//         }
+//       },
+//       {
+//         $match: {
+//           $and: [
+//             {
+//               createdAt: {
+//                 $gte: startDate,
+//                 $lte: endDate
+//               }
+//             },
+//             { template: mongoose.Types.ObjectId(req.query.template) }
+//           ]
+//         }
+//       },
+//       {
+//         $addFields: {
+//           roundedTimestamp: {
+//             $dateTrunc: {
+//               date: '$createdAt',
+//               unit,
+//               binSize
+//             }
+//           },
+//           numericValue: {
+//             $convert: { input: '$value', to: 'double', onError: 0 }
+//           },
+//           variableName: '$variable.name',
+//           variableUnit: '$variable.unit'
+//         }
+//       },
+//       {
+//         $group: {
+//           _id: {
+//             variableName: '$variableName',
+//             variableUnit: '$variableUnit',
+//             roundedTimestamp: '$roundedTimestamp'
+//           },
+//           avgValue: {
+//             $avg: '$numericValue'
+//           }
+//         }
+//       },
+//       {
+//         $group: {
+//           _id: '$_id.roundedTimestamp',
+//           units: {
+//             $push: {
+//               k: '$_id.variableName',
+//               v: '$_id.variableUnit'
+//             }
+//           },
+//           measurements: {
+//             $push: {
+//               k: '$_id.variableName',
+//               v: '$avgValue'
+//             }
+//           }
+//         }
+//       },
+//       {
+//         $project: {
+//           _id: 0,
+//           timestamp: '$_id',
+//           units: {
+//             $arrayToObject: '$units'
+//           },
+//           measurements: {
+//             $arrayToObject: '$measurements'
+//           }
+//         }
+//       },
+//       {
+//         $sort: {
+//           timestamp: 1
+//         }
+//       }
+//     ]
 
-    const queryResult = await Measure.aggregate(pipeline)
+//     const queryResult = await Measure.aggregate(pipeline)
 
-    const results = []
+//     const results = []
 
-    let lowerBound = startDate
-    let upperBound = new Date(lowerBound.getTime() + interval)
+//     let lowerBound = startDate
+//     let upperBound = new Date(lowerBound.getTime() + interval)
 
-    while (lowerBound < endDate) {
-      const intervalResults = queryResult.filter(
-        (item) =>
-          item.timestamp >= lowerBound.getTime() &&
-          item.timestamp < upperBound.getTime()
-      )
+//     while (lowerBound < endDate) {
+//       const intervalResults = queryResult.filter(
+//         (item) =>
+//           item.timestamp >= lowerBound.getTime() &&
+//           item.timestamp < upperBound.getTime()
+//       )
 
-      results.push(...intervalResults)
+//       results.push(...intervalResults)
 
-      lowerBound = upperBound
-      upperBound = new Date(lowerBound.getTime() + interval)
-    }
+//       lowerBound = upperBound
+//       upperBound = new Date(lowerBound.getTime() + interval)
+//     }
 
-    const endTime = performance.now() // Registra el tiempo de finalización
-    const duration = endTime - startTime // Calcula la duración en milisegundos
-    console.log(
-      '🚀 ~ file: measure.controller.js:251 ~ getByPeriod ~ duration:',
-      duration
-    )
+//     const endTime = performance.now() // Registra el tiempo de finalización
+//     const duration = endTime - startTime // Calcula la duración en milisegundos
+//     console.log(
+//       '🚀 ~ file: measure.controller.js:251 ~ getByPeriod ~ duration:',
+//       duration
+//     )
 
-    res.json(results)
-  } catch (err) {
-    console.log(err)
-    return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
-  }
-}
+//     res.json(results)
+//   } catch (err) {
+//     console.log(err)
+//     return res.status(400).json({
+//       error: errorHandler.getErrorMessage(err)
+//     })
+//   }
+// }
 
 /**
  * Middleware to handle requests for a specific measure
